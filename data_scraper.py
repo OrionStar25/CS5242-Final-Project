eef1893132143818d51840c70f53ee524a0dbdbe
Niharika Shrivastava <chunnushrivastava@gmail.com>
2022-10-26 20:03:06 +0800

    Modify to get full summaries

--- data_scraper.py
+++ data_scraper.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import os
 
 from bs4 import BeautifulSoup
 from time import sleep
@@ -13,17 +14,21 @@ genres = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime',
           'History', 'Horror', 'Music', 'Musical', 'Mystery', 'News', 'Reality-TV', 
           'Romance', 'Sci-Fi', 'Sport', 'Talk-Show', 'Thriller', 'War', 'Western']
 pages = 40
-all_titles = []
-all_genres = []
-all_descriptions = []
+SEE_FULL_SUMMARY = 'See full summary'
+PLOT_SUMMARY = 'plotsummary'
+HTML_PARSER = "html.parser"
 
 for genre in genres:
     print("Genre:", genre)
+    all_titles = []
+    all_genres = []
+    all_descriptions = []
+    
     for page in range(pages):
         print("Starting page", page+1)
         url = base_url + genre + '&start=' + str(page*250+1)
         response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.text, "html.parser")
+        soup = BeautifulSoup(response.text, HTML_PARSER)
 
         titles = [a.get_text().strip() for a in soup.select('h3.lister-item-header a')]
         all_titles.extend(titles)
@@ -31,20 +36,36 @@ for genre in genres:
         genres = [a.get_text().strip().replace(' ', '') for a in soup.select('p.text-muted span[class=genre]')]
         all_genres.extend(genres)
 
-        descriptions = [a.get_text().strip() for i, a in enumerate(soup.select('p.text-muted')) if i%2 != 0]
+        descriptions = []
+        summary_links = [b for b in [a.attrs.get('href') for a in soup.select('p.text-muted a')] if PLOT_SUMMARY in b]
+        count = 0
+        for i, a in enumerate(soup.select('p.text-muted')):
+            if i%2 != 0:
+                text = a.get_text().strip()
+                if SEE_FULL_SUMMARY in text:
+                    summary_url = 'https://www.imdb.com' + summary_links[count] 
+                    summary_response = requests.get(summary_url, headers=headers)
+                    summary_soup = BeautifulSoup(summary_response.text, HTML_PARSER)
+
+                    text = [a.get_text().strip() for a in summary_soup.select('li.ipl-zebra-list__item p')][0]
+                    count += 1
+                
+                descriptions.append(text)
+        
         all_descriptions.extend(descriptions)
 
         print("This page done!")
-        sleep(randint(8, 15))
+        sleep(randint(5, 10))
 
-database = []
-for i in range(len(all_titles)):
-    data = {
-        'movie': all_titles[i],
-        'genre': all_genres[i],
-        'summary': all_descriptions[i]}
+    database = []
+    for i in range(len(all_titles)):
+        data = {
+            'movie': all_titles[i],
+            'genre': all_genres[i],
+            'summary': all_descriptions[i]}
 
-    database.append(data)
+        database.append(data)
 
-df = pd.DataFrame(database)
-df.to_csv('imdb_all_genres_data.csv', index=True)
+    df = pd.DataFrame(database)
+    output_path='imdb_all_genres_data.csv'
+    df.to_csv(output_path, mode='a', header=not os.path.exists(output_path))
