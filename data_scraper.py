2b67b710e748478ab26bbac2c50565bc8f006b8c
Niharika Shrivastava <chunnushrivastava@gmail.com>
2022-11-04 20:05:05 +0800

    modify script to store in batch

--- data_scraper.py
+++ data_scraper.py
@@ -9,20 +9,15 @@ from random import randint
 
 base_url = 'https://www.imdb.com/search/title/?title_type=feature,documentary&languages=en&count=250&genres='
 headers = {'Accept-Language': 'en-US,en;q=0.8'}
-genres = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 
-          'Documentary', 'Drama', 'Family', 'Fantasy', 'Film-Noir', 'Game-Show', 
-          'History', 'Horror', 'Music', 'Musical', 'Mystery', 'News', 'Reality-TV', 
-          'Romance', 'Sci-Fi', 'Sport', 'Talk-Show', 'Thriller', 'War', 'Western']
+genres = ['Crime', 'Documentary', 'Drama']
 pages = 40
 SEE_FULL_SUMMARY = 'See full summary'
 PLOT_SUMMARY = 'plotsummary'
 HTML_PARSER = "html.parser"
+output_path='data/labelled_imdb.csv'
 
 for genre in genres:
-    print("Genre:", genre)
-    all_titles = []
-    all_genres = []
-    all_descriptions = []
+    print("--------------------------------- Genre:", genre)
     
     for page in range(pages):
         print("Starting page", page+1)
@@ -31,10 +26,8 @@ for genre in genres:
         soup = BeautifulSoup(response.text, HTML_PARSER)
 
         titles = [a.get_text().strip() for a in soup.select('h3.lister-item-header a')]
-        all_titles.extend(titles)
 
         genres = [a.get_text().strip().replace(' ', '') for a in soup.select('p.text-muted span[class=genre]')]
-        all_genres.extend(genres)
 
         descriptions = []
         summary_links = [b for b in [a.attrs.get('href') for a in soup.select('p.text-muted a')] if PLOT_SUMMARY in b]
@@ -51,21 +44,20 @@ for genre in genres:
                     count += 1
                 
                 descriptions.append(text)
-        
-        all_descriptions.extend(descriptions)
+
+        database = []
+        for i in range(len(titles)):
+            data = {
+                'movie': titles[i],
+                'genre': genres[i],
+                'summary': descriptions[i],
+                'labelled_genre': genre
+            }
+
+            database.append(data)
+
+        df = pd.DataFrame(database)
+        df.to_csv(output_path, mode='a', header=not os.path.exists(output_path))
 
         print("This page done!")
         sleep(randint(5, 10))
-
-    database = []
-    for i in range(len(all_titles)):
-        data = {
-            'movie': all_titles[i],
-            'genre': all_genres[i],
-            'summary': all_descriptions[i]}
-
-        database.append(data)
-
-    df = pd.DataFrame(database)
-    output_path='imdb_all_genres_data.csv'
-    df.to_csv(output_path, mode='a', header=not os.path.exists(output_path))
